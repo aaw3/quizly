9bbd56bb4cd78e23ea2b5ff900f6eb804379325f
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-11-16 13:05:53 -0600

    added joingame path

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import JoinGame from "./pages/JoinGame";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/joingame" element={<JoinGame />} />
       </Routes>
     </Router>
   );
