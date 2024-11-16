0a4d4a495e0d5b6060898b9a4dfea583e7006af2
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-11-16 15:58:39 -0600

    modified bg-color

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,7 +4,7 @@ import LearningCards from "../components/LearningCards";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-800 pb-20">
+    <div className="min-h-screen bg-gray-50 text-gray-800 pb-20">
       <Header />
       <Hero />
       <div className="relative z-10">
